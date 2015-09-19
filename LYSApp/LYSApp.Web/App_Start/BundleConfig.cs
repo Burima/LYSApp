3fbdfd52afb03c314798ef93e6e028102f5f3db8
Goyesh <goyesh.miah@gmail.com>
2015-09-19 17:41:38 +0530

    Bundled all scripts

--- LYSApp/LYSApp.Web/App_Start/BundleConfig.cs
+++ LYSApp/LYSApp.Web/App_Start/BundleConfig.cs
@@ -8,11 +8,33 @@ namespace LYSApp.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //jquery
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.validate*"));
+            //juqery-ui
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui.min.js"));
+
+            //jqueryuitouchpunch
+            bundles.Add(new ScriptBundle("~/bundles/jqueryuitouchpunch").Include(
+                       "~/Scripts/jquery-ui-touch-punch.js"));
+
+            //jqueryplaceholder
+            bundles.Add(new ScriptBundle("~/bundles/jqueryplaceholder").Include(
+                       "~/Scripts/jquery.placeholder.js"));
+
+            //jquerytouchSwipe
+            bundles.Add(new ScriptBundle("~/bundles/jquerytouchSwipe").Include(
+                       "~/Scripts/jquery.touchSwipe.min.js"));
+
+            //infobox
+            bundles.Add(new ScriptBundle("~/bundles/infobox").Include(
+                       "~/Scripts/infobox.js"));
+
+            //jqueryvisible
+            bundles.Add(new ScriptBundle("~/bundles/jqueryvisible").Include(
+                      "~/Scripts/jquery.visible.js"));
 
             // validate 
             bundles.Add(new ScriptBundle("~/bundles/validate").Include(
@@ -25,8 +47,7 @@ namespace LYSApp.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
@@ -34,7 +55,7 @@ namespace LYSApp.Web
 
             // LockYourStay script
             bundles.Add(new ScriptBundle("~/bundles/lockyourstay").Include(
-                      "~/Scripts/Custom/lockyourstay.js"));
+                      "~/Scripts/lockyourstay.js"));
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
             BundleTable.EnableOptimizations = true;
