9b10022f7e08d3b1e39b15bcd5cd069662d0eee1
Goyesh <goyesh.miah@gmail.com>
2015-09-19 17:52:47 +0530

    Added StaticContentConfig to store static data

---
+++ LYSApp/LYSApp.Web/App_Start/StaticContentConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using LYSApp.Domain.StaticContentManagement;
+
+namespace LYSAdmin.Web
+{
+    public class StaticContentConfig
+    {
+        public static void GetStaticContents()
+        {
+            //StaticContentManagement staticContentManagement = new StaticContentManagement();
+            ////storing all Cities and Areas in Application variable
+            //HttpContext.Current.Application["Cities"] = staticContentManagement.GetAllCities();
+            //HttpContext.Current.Application["Areas"] = staticContentManagement.GetAllAreas();
+        }
+
+    }
+}
