3fd7588374fb3bca2d471105bbdb0a38a78f8215
Goyesh <goyesh.miah@gmail.com>
2015-09-20 19:12:46 +0530

    Submitting solution

--- LYSApp/LYSApp.Web/Controllers/AccountController.cs
+++ LYSApp/LYSApp.Web/Controllers/AccountController.cs
@@ -146,7 +146,7 @@ namespace LYSApp.Web.Controllers
             }
 
             // If we got this far, something failed, redisplay form
-            return View(model);
+            return View("Index",model);
         }
 
         //
